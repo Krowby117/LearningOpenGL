5c21a8f248994e090067bc3fdc703fc2a76e48af
Gavin Dominique <gavindom315@gmail.com>
2025-11-02 18:55:49 -0600

    created repositoru

---
+++ LearningOpenGL/src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  LearningOpenGL
+//
+//  Created by Gavin Dominique on 11/2/25.
+//
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Fresh GLAD project setup!" << std::endl;
+    return 0;
+}
